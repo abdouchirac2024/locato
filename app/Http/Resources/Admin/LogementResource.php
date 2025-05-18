4277dc1c336d9ee37ea12af353242b04ccc604c6
KENMEGNE YOH IVANA <yohivana237@gamail.com>
2025-05-18 16:12:33 +0100

    good Class App\Http\Resources\LogementResource located in ./app/Http/Resources/Admin/TypeLogementResource.php does not comply with psr-4 autoloading standard (rule: App\ => ./app). Skipping. > Illuminate\Foundation\ComposerScripts::postAutoloadDump

--- app/Http/Resources/Admin/LogementResource.php
+++ app/Http/Resources/Admin/LogementResource.php
@@ -1,15 +1,9 @@
 <?php
 
-namespace App\Http\Resources; // Le namespace de cette classe reste le même
+namespace App\Http\Resources\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-
-// --- MODIFICATION : Importer la ressource spécifique depuis le dossier Admin ---
-use App\Http\Resources\Admin\TypeLogementResource;
-// --- FIN MODIFICATION ---
-
-// Importer les autres ressources nécessaires (assurez-vous qu'elles existent et sont correctes)
 use App\Http\Resources\QuartierResource;
 use App\Http\Resources\BailleurResource;
 use App\Http\Resources\ImageLogementResource;
@@ -29,15 +23,10 @@ class LogementResource extends JsonResource
             return [];
         }
 
-        // La locale (fr/en) est définie dans le contrôleur avant d'appeler cette ressource.
-        // Les accesseurs du modèle Logement ($this->description, $this->dispo, $this->libelle)
-        // et les accesseurs du modèle TypeLogement via la ressource importée
-        // retourneront donc automatiquement la bonne langue.
-
         return [
             'id' => $this->id,
             'reference' => $this->reference,
-            'libelle' => $this->libelle, // Utilise l'accesseur getLibelleAttribute() du modèle Logement
+            'libelle' => $this->libelle,
             'coordonnees' => [
                 'latitude' => $this->latitude,
                 'longitude' => $this->longitude,
@@ -52,7 +41,7 @@ class LogementResource extends JsonResource
             ],
             'prix_mensuel' => $this->prix,
             'mois_avance_requis' => $this->nbrMois,
-            'description' => $this->description, // Utilise l'accesseur getDescriptionAttribute()
+            'description' => $this->description,
             'equipements' => [
                 'compteur_prepaye' => (bool) $this->contPrep,
                 'forage' => (bool) $this->forage,
@@ -60,7 +49,7 @@ class LogementResource extends JsonResource
                 'gardien' => (bool) $this->gardien,
                 'climatisation' => (bool) $this->climatisation,
             ],
-            'disponibilite' => $this->dispo, // Utilise l'accesseur getDispoAttribute()
+            'disponibilite' => $this->dispo,
             'statut_validation' => $this->validation_status,
             'statut_validation_lisible' => match ($this->validation_status) {
                  \App\Models\Logement::STATUS_EN_ATTENTE => 'En attente',
@@ -68,21 +57,14 @@ class LogementResource extends JsonResource
                  \App\Models\Logement::STATUS_REJETE => 'Rejeté',
                  default => 'Inconnu',
              },
-            'notes_admin' => $this->admin_notes, // Inclure les notes
-
-            // --- MODIFICATION : Utilise la classe importée ---
-            // PHP sait maintenant que TypeLogementResource fait référence à App\Http\Resources\Admin\TypeLogementResource
+            'notes_admin' => $this->admin_notes,
             'type_logement' => new TypeLogementResource($this->whenLoaded('typeLogement')),
-            // --- FIN MODIFICATION ---
-
             'quartier_details' => new QuartierResource($this->whenLoaded('quartier')),
             'bailleur_info' => new BailleurResource($this->whenLoaded('bailleur')),
             'images' => ImageLogementResource::collection($this->whenLoaded('imagesLogement')),
             'avis_recents' => AvisResource::collection($this->whenLoaded('avis')),
-
-            // Timestamps
             'date_creation' => $this->created_at?->toIso8601String(),
             'derniere_modification' => $this->updated_at?->toIso8601String(),
         ];
     }
-}
+} 
